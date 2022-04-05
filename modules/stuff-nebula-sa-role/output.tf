73c28407ff022301ceda6d463196d130893ed74d
tennengabr <79433169+tennengabr@users.noreply.github.com>
2022-04-05 16:48:36 +1200

    Update output.tf

--- modules/stuff-nebula-sa-role/output.tf
+++ modules/stuff-nebula-sa-role/output.tf
@@ -7,3 +7,8 @@ output "id" {
   description = "ID of the service account generated"
   value       = aws_iam_role.app_service_account.id
 }
+
+output "arn" {
+  description = "ARN for service account role"
+  value       = aws_iam_role.app_service_account.arn
+}
