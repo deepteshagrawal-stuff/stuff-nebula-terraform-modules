74f101149e4cb870c3049a0d8d2e84f2e6a47f03
David Mote <david.mote@integration.works>
2022-04-13 08:00:59 +1200

    Add content based deduplication flag

--- stuff-nebula-sqs/main.tf
+++ stuff-nebula-sqs/main.tf
@@ -1,6 +1,7 @@
 resource "aws_sqs_queue" "sqs" {
-  name                      = var.queue_name
-  delay_seconds             = var.delay_seconds
-  message_retention_seconds = var.message_retention
-  fifo_queue                = var.fifo_queue
+  name                        = var.queue_name
+  delay_seconds               = var.delay_seconds
+  message_retention_seconds   = var.message_retention
+  fifo_queue                  = var.fifo_queue
+  content_based_deduplication = true
 }
