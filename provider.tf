b433ca27a4b1613531e94d4fc51e3f6548bbb814
Gabriel Tennent <gabriel.tennent@integration.works>
2022-06-02 12:00:26 +1200

    removing the assume role

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   region = "ap-southeast-2"
-  assume_role {
-    session_name = "aws_role_assumed"
-    role_arn     = "arn:aws:iam::545828170065:user/nebula-base-terraform-modules-sa"
-  }
+  # assume_role {
+  #   session_name = "aws_role_assumed"
+  #   role_arn     = "arn:aws:iam::545828170065:user/nebula-base-terraform-modules-sa"
+  # }
 }
 
 terraform {
