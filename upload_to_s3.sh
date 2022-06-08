576b34c2b87c9a88ee7d92fea83a5726d4b21bfe
Gabriel Tennent <gabriel.tennent@integration.works>
2022-06-08 14:31:23 +1200

    Updating the upload_to_s3 script to use raw bucket name input

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -1,4 +1,4 @@
-export BUCKET_NAME=$(terraform output bucket_id)
+export BUCKET_NAME=$(terraform output -raw bucket_id)
 for module in ./zips/*; do
     for version in ./zips/$(basename $module)/*; do
         aws s3 cp ./zips/$(basename $module)/* s3://$BUCKET_NAME/$(basename $module)/$(basename $version)
