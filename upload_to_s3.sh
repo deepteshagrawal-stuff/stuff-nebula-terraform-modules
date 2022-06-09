68318f50b55806f50a933161e1cea5352fc6e1f8
Gabriel Tennent <gabriel.tennent@integration.works>
2022-06-10 09:45:08 +1200

    preventing script from trying to access files that don't exist - fixing syntax

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -1,10 +1,8 @@
 export BUCKET_NAME=$(terraform output -raw bucket_id)
 if [ -d ./zips/* ]; then 
     for module in ./zips/*; do
-        # Add check for if this folder exists
         for version in ./zips/$(basename $module)/*; do
-            # aws s3 cp ./zips/$(basename $module)/* s3://$BUCKET_NAME/$(basename $module)/$(basename $version)
-            echo ./zips/$(basename $module)
+            aws s3 cp ./zips/$(basename $module)/* s3://$BUCKET_NAME/$(basename $module)/$(basename $version)
         done
     done
 fi
