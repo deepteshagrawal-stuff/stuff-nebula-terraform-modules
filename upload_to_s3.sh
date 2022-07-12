70233d9cf7bae9d7bfd56cfa1165ec20f2bf3128
Gabriel Tennent <gabriel.tennent@integration.works>
2022-07-13 11:25:36 +1200

    Adding printing of full path. Readding the new modules

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -1,11 +1,12 @@
 # export BUCKET_NAME=$(terraform output -raw bucket_id)
 echo "$(ls zips)"
 echo "$(ls zips/*)"
+echo "$(ls zips/*/*)"
 if [ -d ./zips/* ]; then 
     for module in ./zips/*; do
-        echo "Module found : $module"
+        # echo "Module found : $module"
         for version in ./zips/$(basename $module)/*; do
-            echo "Version of $module found: $version"
+            # echo "Version of $module found: $version"
             aws s3 cp ./zips/$(basename $module)/* s3://$BUCKET_NAME/$(basename $module)/$(basename $version)
         done
     done
