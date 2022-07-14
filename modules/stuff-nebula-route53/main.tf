df6c787cebd165981a690eb59db14efd2260ec6c
Gabriel Tennent <gabriel.tennent@integration.works>
2022-07-14 15:32:32 +1200

    Pushing up changes to support aliases better

--- modules/stuff-nebula-route53/main.tf
+++ modules/stuff-nebula-route53/main.tf
@@ -2,16 +2,10 @@ resource "aws_route53_record" "route53-record" {
   zone_id = var.hosted_zone
   name    = var.record_name
   type    = var.record_type
-  ttl     = var.ttl
 
-  records = [
-    {
-      name    = var.alias
-      type    = var.record_type
-      alias   = {
-        name    = var.alias
-        zone_id = var.hosted_zone   
-      }
-    }
-  ]
+  alias {
+    name                   = var.alias
+    zone_id                = var.elb_hosted_zone
+    evaluate_target_health = false
+  }
 }
