553784409ac1b6a77c1c860b7539fcf53edd49f8
adam-kwiecien-iw <adam.kwiecien@integration.works>
2022-10-04 14:59:50 +1300

    removing rds object from variables

--- modules/stuff-nebula-rds/main.tf
+++ modules/stuff-nebula-rds/main.tf
@@ -1,10 +1,10 @@
 resource "aws_db_subnet_group" "default" {
-  name       = "${var.rds_object.db_name}_subnet_group"
+  name       = "${var.db_name}_subnet_group"
   subnet_ids = var.subnet_ids
 }
 
 resource "aws_security_group" "rds_sg" {
-  name        = "${var.rds_object.identifier}-rds-security-group"
+  name        = "${var.identifier}-rds-security-group"
   description = "Security group associated with the RDS"
   vpc_id      = var.vpc_id
 
@@ -17,15 +17,15 @@ resource "aws_security_group" "rds_sg" {
 }
 
 resource "aws_db_instance" "rds" {
-  db_name                     = var.rds_object.db_name
-  identifier                  = var.rds_object.identifier
-  allocated_storage           = var.rds_object.allocated_storage
-  engine                      = var.rds_object.engine
-  engine_version              = var.rds_object.engine_version
-  instance_class              = var.rds_object.instance_class
-  username                    = var.rds_object.username
+  db_name                     = var.db_name
+  identifier                  = var.identifier
+  allocated_storage           = var.allocated_storage
+  engine                      = var.engine
+  engine_version              = var.engine_version
+  instance_class              = var.instance_class
+  username                    = var.username
   password                    = var.rds_password
-  skip_final_snapshot         = var.rds_object.skip_final_snapshot
+  skip_final_snapshot         = var.skip_final_snapshot
   vpc_security_group_ids      = [aws_security_group.rds_sg.id]
   db_subnet_group_name        = aws_db_subnet_group.default.name
   allow_major_version_upgrade = false
