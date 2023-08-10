5f2d3a25e9ebfc23351643ddd535888b801fb076
adam-kwiecien-iw <adam.kwiecien@integration.works>
2023-08-11 11:23:08 +1200

    updating modules

--- modules/stuff-nebula-secrets-manager-secret/main.tf
+++ modules/stuff-nebula-secrets-manager-secret/main.tf
@@ -4,6 +4,6 @@ resource "aws_secretsmanager_secret" "secret" {
 }
 
 resource "aws_secretsmanager_secret_version" "secret_version" {
-  secret_id     = var.secret_id
+  secret_id     = aws_secretsmanager_secret.secret.id
   secret_string = var.secret_string
 }
