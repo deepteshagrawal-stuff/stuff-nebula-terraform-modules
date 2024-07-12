2f5e4477a73a1e6b6d31739b2677d26bbb6763d4
Deeptesh Agrawal <deeptesh.agrawal@stuff.co.nz>
2024-07-12 17:33:11 +1200

    Fixing validation errors.

--- modules/stuff-aws-lambda/main.tf
+++ modules/stuff-aws-lambda/main.tf
@@ -12,7 +12,7 @@ resource "aws_lambda_function" "lambda_function" {
   role             = var.function_role
   handler          = var.function_handler
   runtime          = var.function_runtime
-  layers           = var.create_lambda_layer ? [aws_lambda_layer_version.lambda_layer.arn] : []
+  layers           = var.create_lambda_layer ? [aws_lambda_layer_version.lambda_layer[0].arn] : []
   timeout          = var.function_timeout
 
   # Environment variables
@@ -52,18 +52,20 @@ data "aws_iam_policy_document" "lambda_cloudwatch_policy" {
 
 locals {
   inline_policies = jsonencode({
-                      cloudwatch_logging_policy = {
-                        name   = "cloudwatch_logging_policy"
-                        policy = data.aws_iam_policy_document.lambda_cloudwatch_policy.json
-                      }
-                    })
+    default = {
+      cloudwatch_logging_policy = {
+        name   = "cloudwatch_logging_policy"
+        policy = data.aws_iam_policy_document.lambda_cloudwatch_policy.json
+      }
+    }
+  })
 }
 
 module "iamrole_policy_module" {
   source                  = "../stuff-aws-iamrole-policy/"
   create_role             = true
-  role_name               = var.function_name + "_lambda_role"
-  role_description        = "Lambda role for " + var.function_name
+  role_name               = "${var.function_name}_lambda_role"
+  role_description        = "Lambda role for ${var.function_name}"
   role_service            = "lambda.amazonaws.com"
   inline_policies         = local.inline_policies
   create_policy           = false
