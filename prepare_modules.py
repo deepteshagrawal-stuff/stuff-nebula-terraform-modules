d94cd161033c83254a7c140846c35cfb0cbea46c
Deeptesh Agrawal <deeptesh.agrawal@stuff.co.nz>
2024-07-22 16:19:33 +1200

    Reverting back all the changes and keeping the file hierarchy for dependant modules.

--- prepare_modules.py
+++ prepare_modules.py
@@ -8,72 +8,55 @@ modules_dir = './modules'
 zip_dir = './zips'
 
 def main():
-    print("Inside main function")
-    if os.path.exists(zip_dir):
-        print("Zips director exists - removing zips directory")
-        os.system("rm -rf " + zip_dir)
     os.mkdir(zip_dir)
     #Iterate modules directory
     for module in os.listdir(modules_dir):
-        try:
-            print("Analysing Module: " + module)
-            module_dir = str(os.path.join(modules_dir, module))
-            version_file = module_dir + "/version-history.json"
-            if os.path.isfile(version_file):
-                # Loads version file
-                with open(version_file, "r") as f:
-                    data = json.load(f)
-                f.close()
-                # Ensures version set for this release is not included in past versions
-                if not data["current-version"] in data["versions"] and not data["current-version"] == "":
-                    print(module + ": release version set... preparing for release...")
-                    current_version = data["current-version"]
-                    module_zip_dir = os.path.join(zip_dir, module)
-                    os.mkdir(module_zip_dir)
-                    zip_folder(current_version, module_dir, module_zip_dir)
-                    print(module + ": Zippped successfully...")
-                    update_versions_file(version_file, data)
-                    print(module + ": Version file updated! New version: " + current_version)
-                else:
-                    print(module + ":No version set or the version already exists!")
+        print("Analysing Module: " + module)
+        module_dir = str(os.path.join(modules_dir, module))
+        version_file = module_dir + "/version-history.json"
+        if os.path.isfile(version_file):
+            # Loads version file
+            with open(version_file, "r") as f:
+                data = json.load(f)
+            f.close()
+            # Ensures version set for this release is not included in past versions
+            if not data["current-version"] in data["versions"] and not data["current-version"] == "":
+                print(module + ": release version set... preparing for release...")
+                current_version = data["current-version"]
+                module_zip_dir = os.path.join(zip_dir, module)
+                os.mkdir(module_zip_dir)
+                zip_folder(current_version, module_dir, module_zip_dir)
+                print(module + ": Zippped successfully...")
+                update_versions_file(version_file, data)
+                print(module + ": Version file updated! New version: " + current_version)
             else:
-                print(module + ": No version file!")
-        except Exception as e:
-            print(f"unexpection exception in main: {e}")
+                print(module + ":No version set or the version already exists!")
+        else:
+            print(module + ": No version file!")
 
 # Zips folder for release to Amazon s3
 def zip_folder(current_version, module_dir, module_zip_dir):
-    try:
-        file_name = current_version+".zip"
-        zip_file = os.path.join(module_zip_dir,file_name)
-        with ZipFile(zip_file, 'w') as zipObj:
-            print(f"Created ZIP file path: {zip_file}")
-            for folderName, subFolders, fileNames in os.walk(module_dir):
-                print(f"folderName: {folderName}")
-                for fileName in fileNames:
-                    if(fileName.endswith(".tf")):
-                        filePath = os.path.join(folderName, fileName)
-                        print(f"filePath: {filePath}")
-                        arcname = os.path.relpath(filePath, module_dir)
-                        print(f"relative file name: {arcname}")
-                        zipObj.write(filePath, arcname)
-    except Exception as e:
-        print(f"zipping folder exception: {e}")
+    file_name = current_version+".zip"
+    zip_file = os.path.join(module_zip_dir, file_name)
+    with ZipFile(zip_file, 'w') as zipObj:
+        print(f"created zip file: {zip_file}")
+        for folderName, subFolders, fileNames in os.walk(module_dir):
+            for fileName in fileNames:
+                if(fileName.endswith(".tf")):
+                    filePath = os.path.join(folderName, fileName)
+                    print(f"filePath: {filePath}")
+                    # below one is done to add different modules keeping file hierarchy: .terraform/modules/<module_name>/main.tf
+                    arcname = os.path.relpath(filePath, module_dir)
+                    print(f"relative file name: {arcname}")
+                    zipObj.write(filePath, arcname)
 
 # Modifys the version=history.json to include new version as a version
 def update_versions_file(version_file, data):
-    try:
-        data["versions"].append(data["current-version"])
-        data["current-version"] = ""
-        with open(version_file, "w") as f:
-            json.dump(data, f, indent=4)
-        f.close()
-    except Exception as e:
-        print(f"update versions file exception: {e}")
+    data["versions"].append(data["current-version"])
+    data["current-version"] = ""
+    with open(version_file, "w") as f:
+        json.dump(data, f, indent=4)
+    f.close()
 
 if __name__ == "__main__":
-    try:
-        print("calling main")
-        main()
-    except Exception as e:
-        print(f"exception while calling main: {e}")
+    main()
